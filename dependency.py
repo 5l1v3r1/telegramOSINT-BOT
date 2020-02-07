54eae8395f9f5efb8c5a7bd7267831efca109f19
danilpalageychenko <_dani4_@mail.ru>
2020-02-07 16:37:29 +0200

    Update dependency.py

--- dependency.py
+++ dependency.py
@@ -1,5 +1,5 @@
-token = '970058233:AAEMsgR7Yd2Psuadw1dMERgSmHQMVe1QeWc'
-telegramID = ['654753898', '341876831']
-api_id = 1093859
-api_hash = '522089ed4f1c11814c8af05268e944b3'
-passwordFor2Factor = "Qwerty123"
+token = '...'
+telegramID = ['...', '...']
+api_id = ...
+api_hash = '...'
+passwordFor2Factor = "..."
