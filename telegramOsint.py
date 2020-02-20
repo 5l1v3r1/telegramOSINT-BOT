d5aa10cc566ced969bd610774d89701b0cc13b7c
danilpalageychenko <_dani4_@mail.ru>
2020-02-20 15:52:59 +0200

    Update telegramOsint.py

--- telegramOsint.py
+++ telegramOsint.py
@@ -18,10 +18,14 @@ def send_text(message):
     if str(message.from_user.id) not in telegramID:
         bot.send_message(message.chat.id, 'False')
         exit()
-    if re.match(r"^((8|\+\d{1})[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$", message.text) != None:
-        subprocess.run(["python3", "client.py", message.text, str(message.chat.id)], stdout=subprocess.DEVNULL)
-        #subprocess.run(["python3", "client.py", message.text, str(message.chat.id)])
+    if re.match(r"^((8|\+\d{3})[\- ]?)?(\(?\d{2}\)?[\- ]?)?[\d\- ]{7,10}$", message.text) != None:
+        subprocess.run(["python", "client.py", message.text, str(message.chat.id)], stdout=subprocess.DEVNULL)
+        #subprocess.run(["python", "client.py ", message.text, str(message.chat.id)])
     elif message.text:
-        bot.send_message(message.chat.id, "wrong number, re-enter")
+        bot.send_message(message.chat.id, """Неправильный формат номера, попробуйте ввестифыаф заново!\nУкажи телефон в любом формате, например: 
++38023334455
++380(22)333-44-55
+380(23)333-44-55
+380 23 333 44 55""")
 
-bot.polling()
+bot.polling(none_stop=True)
